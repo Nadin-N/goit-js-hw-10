1b27bae507e1a9a6b7a0717fa7a1e4395781b6e2
Nadin Severin <nadinlikes2018@gmail.com>
2023-01-12 02:43:18 +0200

    add changes

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+const fields = 'fields=name,capital,population,flags,languages';
+
+function fetchCountries(countryName) {
+  return fetch(`${BASE_URL}/name/${countryName}?${fields}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    //  console.log(response.json());
+    return response.json();
+  });
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputSearchCountryEl: document.querySelector('input#search-box'),
+};
+
+// console.log(refs.inputSearchCountryEl);
+// fetchCountries('ukraine');
+
+refs.inputSearchCountryEl.addEventListener(
+  'input',
+  debounce(onCountryInput, DEBOUNCE_DELAY)
+);
+
+function onCountryInput() {
+  const countryName = refs.inputSearchCountryEl.value.trim();
+  console.log(countryName);
+
+  fetchCountries(countryName)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  //   fetchCountries(countryName);
+}
+
+// function renderCountryInfo(countryName) {
+// 	const markup = `
+// <h2></h2>
+// <p>Capital</p>
+// <p>Population</p>
+// <p>Capital</p>
+// 	`;
+// }
